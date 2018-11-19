4b41e4b2102e1d11b34a5f0d51dd3016caf523af
Donald Wasserman <djwasserman@gmail.com>
2018-11-19 14:30:50 -0500

    delete old test

--- test/index.test.js
+++ test/index.test.js
@@ -19,25 +19,7 @@ describe('My Probot app', () => {
     // just return a test token
     app.app = () => 'test'
   })
-
-  test('creates a comment when an issue is opened', async () => {
-    // Test that we correctly return a test token
-    nock('https://api.github.com')
-      .post('/app/installations/2/access_tokens')
-      .reply(200, { token: 'test' })
-
-    // Test that a comment is posted
-    nock('https://api.github.com')
-      .post('/repos/hiimbex/testing-things/issues/1/comments', (body) => {
-        expect(body).toMatchObject(issueCreatedBody)
-        return true
-      })
-      .reply(200)
-
-    // Receive a webhook event
-    await probot.receive({ name: 'issues', payload })
-  })
-})
+}) 
 
 // For more information about testing with Jest see:
 // https://facebook.github.io/jest/
